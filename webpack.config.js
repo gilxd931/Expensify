e7ae406de4bf6e5cb9a50aa27739e2d672db529d
gil rotem <gilxd931@gmail.com>
2020-10-23 19:57:48 +0300

    setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,55 @@
-
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            enforce: 'pre',
-            use: [
-                'babel-loader',
-                'source-map-loader'
-            ],
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+module.exports = (env) => {
+    const isProd = env === 'production';
+
+    console.log('env: ', env)
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                enforce: 'pre',
+                use: [
+                    'babel-loader',
+                    'source-map-loader'
+                ],
+            }, {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+                ],
+            }
+
             ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ],
+        devtool: isProd ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
         }
-
-        ]
-    },
-    mode: 'development',
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
     }
 };
+
