10622d852b11b1a8d19a32d6fcbdb92e305c1464
gil rotem <gilxd931@gmail.com>
2020-11-03 01:25:41 +0200

    last commit

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,13 +3,19 @@ import { connect } from 'react-redux';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
+import { Link } from 'react-router-dom';
 
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
     const formatedTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
-        <div>
-            <h1>view {expensesCount} {expenseWord} total of {formatedTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">View <span>{expensesCount} </span>{expenseWord} total of <span>{formatedTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link to='/create' className="button">Add epxense</Link>
+                </div>
+            </div>
         </div>
     );
 };
